bc59acc82d74d746353c2a8d994057c47293bab2
Max Skorokhodov <skormaksim@mail.ru>
2023-05-02 23:59:14 +0300

    изменить package-lock

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET, NODE_ENV } = process.env;
 
-const devMode = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
+const key = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
 
-const createToken = (payload) => jwt.sign(payload, devMode, { expiresIn: '7d' });
+const createToken = (payload) => jwt.sign(payload, key, { expiresIn: '7d' });
 
-const verifyToken = (token) => jwt.verify(token, devMode);
+const verifyToken = (token) => jwt.verify(token, key);
 
 module.exports = { createToken, verifyToken };
