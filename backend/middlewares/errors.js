a5115388d77ba4aafd43b878d8aa64f8e4495b29
Max Skorokhodov <skormaksim@mail.ru>
2023-05-04 11:19:41 +0300

    fix: использовать NotFoundError в handleNotFoundUrl

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,10 +1,10 @@
 const http2 = require('node:http2');
+const NotFoundError = require('../errors/NotFoundError');
 
-const NOT_FOUND = http2.constants.HTTP_STATUS_NOT_FOUND; // 404
 const SERVER_ERROR = http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR; // 500
 
-const handleNotFoundUrl = (req, res) => {
-  res.status(NOT_FOUND).send({ message: 'По указанному url ничего нет' });
+const handleNotFoundUrl = (req, res, next) => {
+  next(new NotFoundError('По указанному url ничего нет'));
 };
 
 const handleErrors = (err, req, res, next) => {
